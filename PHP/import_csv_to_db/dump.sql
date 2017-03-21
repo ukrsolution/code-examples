015865ed60f3b017ba8d25097ee1c5387ccb9ab8
Serg G <sgvcoder@gmail.com>
2017-03-21 10:45:26 +0200

    PHP example: import_csv_to_db

---
+++ PHP/import_csv_to_db/dump.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.13-0ubuntu0.16.04.2 : Database - csv_data
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`csv_data` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `csv_data`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(250) DEFAULT NULL,
+  `last_name` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+/*Table structure for table `users_data` */
+
+DROP TABLE IF EXISTS `users_data`;
+
+CREATE TABLE `users_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `users_data_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users_data` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ PHP/import_csv_to_db/index.php
@@ -0,0 +1,61 @@
+<?php 
+
+class ImportUsers{
+
+	private $PDO = null;
+
+	public function __construct () {
+
+		try {
+			// init databse connection
+			$this->PDO = new PDO ("mysql:host=localhost;dbname=csv_data", "root", "123", array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+		}
+	    catch (Exception $e)
+	    {
+	        print_r($e);
+	    }
+	}
+
+	// Load users' data from CSV file
+	public function FromCSV ($file_name) {
+
+		$file = fopen($file_name, 'r');
+		// Reading line by line  - a little bit slower but doesn't require a lot of memory.
+		while (($line = fgetcsv($file)) !== FALSE) {
+
+			// Insert user into DB
+			$this->InsertUser($line);
+		}
+		fclose($file);
+	}
+
+	// Inserting user into database
+	private function InsertUser($user) {
+
+		try {
+	        
+			// Insert into "Users" table
+			$statement = $this->PDO->prepare ("INSERT INTO `users` (`first_name`, `last_name`) VALUES (?, ?);");
+			$statement->bindParam (1, $user[0], PDO::PARAM_STR);
+			$statement->bindParam (2, $user[1], PDO::PARAM_STR);
+			$statement->execute ();
+			
+			// Geting user ID
+			$user_id = $this->PDO->lastInsertId();
+
+			// Inserting additional user's data into "users_data" table
+			$statement = $this->PDO->prepare ("INSERT INTO `users_data` (`user_id`, `email`, `address`) VALUES (?, ?, ?);");
+			$statement->bindParam (1, $user_id, PDO::PARAM_INT);
+			$statement->bindParam (2, $user[4], PDO::PARAM_STR);
+			$statement->bindParam (3, $user[9], PDO::PARAM_STR);
+			$statement->execute ();
+		}
+	    catch (Exception $e)
+	    {
+	        print_r($e);
+	    }
+	}
+}
+
+$ImportUsers = new ImportUsers();
+$ImportUsers->FromCSV('contacts.csv');
